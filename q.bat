581452bd866d670984f3e96be5b1f10f0c10daa8
creeper <wgqcreeper@gmail.com>
2018-01-20 23:40:02 +0800

    new

--- q.bat
+++ q.bat
@@ -2,5 +2,5 @@ git init
 git add -A
 git commit -m "new"
 git push origin master
-git remote add origin https://github.com/wgqcreeper/update.git
+#git remote add origin https://github.com/wgqcreeper/update.git
 git pull origin master
